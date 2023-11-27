d9121f85158bae422a5909256d5009ccc7b0ed2b
iravigangwar <ravigangwar7465@gmail.com>
2023-11-27 21:40:17 +0530

    work on navbar make sticky on scroll

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,32 +1,67 @@
-import { Link } from 'react-router-dom'
-import { FaBucket } from "react-icons/fa6";
-import '../style/navBar.css'
-import { useSelector } from "react-redux"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBucket } from 'react-icons/fa6';
+import { useSelector } from 'react-redux';
+import '../style/navBar.css';
 
 function NavBar() {
-  const bucketState = useSelector((state)=> state.bucketReducer);
+  const bucketState = useSelector((state) => state.bucketReducer);
+
+  // State to track whether the navbar should be sticky
+  const [isSticky, setIsSticky] = useState(false);
+
+  // Effect to handle scroll events and update the isSticky state
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || window.pageYOffset;
+
+      // Check if the user has scrolled 1vh down
+      setIsSticky(scrollY > window.innerHeight * 0);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav id='navBarId'>
+    <nav id="navBarId" className={isSticky ? 'sticky' : ''}>
       <div id="logoId" className="logo">
-      <Link className="link-div" to='/'>
-        <img src="https://github.com/ravi-gangwar/greenEarth/blob/main/frontend/src/assets/green-earth.png?raw=true" alt="" />
-      </Link>
-      </div>
-        <ul>
-            
-            <Link className="link-div" to='/'><li>Home</li></Link>
-            <Link className="link-div" to='/garden'><li>Garden</li></Link>
-            <Link className="link-div" to='/contact'><li>Contact</li></Link>
-            <Link className="link-div" to='/about'><li>About Us</li>  </Link>            
-        </ul>
-      <div className='navBar-btns'>
-        <Link to="/login">
-          <button className='login-btn'>Login</button>
+        <Link className="link-div" to="/">
+          <img src="https://github.com/ravi-gangwar/greenEarth/blob/main/frontend/src/assets/green-earth.png?raw=true" alt="" />
         </Link>
-        <Link to="bucket"> <button className='bucket-btn'><FaBucket className='bucket'/><span>{bucketState.bucketItems.length}</span></button></Link>
+      </div>
+      <ul>
+        <Link className="link-div" to="/">
+          <li>Home</li>
+        </Link>
+        <Link className="link-div" to="/garden">
+          <li>Garden</li>
+        </Link>
+        <Link className="link-div" to="/contact">
+          <li>Contact</li>
+        </Link>
+        <Link className="link-div" to="/about">
+          <li>About Us</li>
+        </Link>
+      </ul>
+      <div className="navBar-btns">
+        <Link to="/login">
+          <button className="login-btn">Login</button>
+        </Link>
+        <Link to="bucket">
+          <button className="bucket-btn">
+            <FaBucket className="bucket" />
+            <span>{bucketState.bucketItems.length}</span>
+          </button>
+        </Link>
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
