1f98c8c99e11514ced40218a5a9267705f24cadb
ravi-gangwar <ravigangwar7465@gmail.com>
2023-12-17 03:08:52 +0530

    added getordered items fuctionalities

---
+++ backend/model/orderModel.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+
+const orderSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'user name is Required'],
+        minLength: [5, "name min length 5 char"],
+        maxLength: [15, "name must be less than 15 char"],
+        trim: true
+    },
+    email : {
+        type : String,
+        require: [true, "user email must be required"],
+        lowercase: true
+    },
+    userId : {
+        type : String,
+        require: [true, "user id must be required"],
+    },
+    orderTrees : [],
+    shippingAddress: {
+        type: Object,
+        require: [true, "shipping address must be required"]
+    },
+    orderAmount : {
+        type: Number,
+        require: [true, "order amount must be required"]
+    },
+    isDeliverd : {
+        type: String,
+        require: [true, "isDeliverd must be required"],
+        default: false
+    },
+    transectionId : {
+        type: String,
+        require: [true, "transectionId must be required"]
+    }
+    
+}, 
+    {
+        timestamps: true
+    }
+)
+
+const orderModel  = mongoose.model('order', orderSchema)
+
+export default orderModel;
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,42 +1,67 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
 import Stripe from 'stripe';
+import orderModel from "../model/orderModel.js"
 
-
-const stripe = new Stripe('sk_test_51OHyz1SB7yI7Si8NED0HEr2OGThJUoUIYHATxbT9SY9ZKuaWI09BoZjGRQ0dVh4HevuS0ahzpNVQwIXAW10lPc4P0018ZXFqp6');
 const orderRouter = express.Router();
+const stripe = new Stripe('sk_test_51OHyz1SB7yI7Si8NED0HEr2OGThJUoUIYHATxbT9SY9ZKuaWI09BoZjGRQ0dVh4HevuS0ahzpNVQwIXAW10lPc4P0018ZXFqp6');
 
 orderRouter.post('/placeorder', async (req, res) => {
-    const { token, total, currentUser, bucketItems } = req.body;
-    try {
-        // Create a customer in Stripe
-        const customer = await stripe.customers.create({
-            email: token.email,
-          });
+    const { bucketItems, address} = req.body;
 
-        // Create a charge for the customer
-        const payment = await stripe.charges.create({
-            amount: total * 100,
+    // Construct line items
+    const lineItems = bucketItems.map((item) => ({
+        price_data: {
             currency: 'inr',
-            customer: customer.id,
-            receipt_email: token.email
-        }, {
-            idempotencyKey: uuidv4()
-        });
-        
-        console.log(payment , "payments");
-        if (payment) {
-            res.status(200).send('Payment Success');
-        } else {
-            res.status(400).send('Payment Failed');
-        }
+            product_data: {
+                name: item.name,
+            },
+            unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+    }));
 
-    } catch (error) {
-        res.status(400).json({
-            message: 'Something went wrong',
-            error: error.stack,
-        });
+    // Create a Checkout Session with the session data
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: 'http://localhost:5173/orders',
+        cancel_url: 'http://localhost:5173/cancel',
+    }); 
+
+    if(session){
+        const newOrder = new orderModel({
+            name: address.name,
+            email: address.email,
+            userId: address.userId,
+            orderTrees: bucketItems,
+            shippingAddress: address,
+            orderAmount: address.amount,
+            transectionId: session.id,
+        })
+        const saveOrder = await newOrder.save();
+        if(saveOrder){
+            console.log("order save");
+        }
+        else{
+            console.log("order not save");
+        }
     }
+
+    res.json({
+        id: session.id,
+    });
 });
+
+
+orderRouter.post('/getorders', async (req, res) => {
+    const { userId } = req.body;
+    try {
+        const orders = await orderModel.find({ userId: userId }).sort({_id: "-1"});
+        res.send(orders);
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 export default orderRouter;
