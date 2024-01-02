4fe15ead8a329e58efd96e5211e0c3aab2f201da
ravi-gangwar <ravigangwar7465@gmail.com>
2024-01-02 05:40:47 +0530

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,14 @@ const app = express();
 // };
 
 // app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://green-earth-x5bm.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors());
 
 app.use(express.json());
